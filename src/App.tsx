e0036560981c764fbfc400a49380dfaa930cd493
Matthieu Bleichner <matthieu258@hotmail.com>
2024-06-19 12:18:04 +0200

    add topMenu bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import Cities from './pages/Cities/Cities';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TopMenu from 'components/TopMenu';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { grey } from '@mui/material/colors';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: grey[100]
+    }
+  }
+});
 function App(): JSX.Element {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Cities />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <TopMenu />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Cities />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 export default App;
